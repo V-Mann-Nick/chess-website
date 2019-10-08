5b3866303faeff78909f98c6a175140b2630474f
Nicklas <n.negotium@posteo.net>
2019-10-08 21:53:57 +0000

    config changed

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
 import os
+import json
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
+try:
+    with open('/etc/config.json') as config_file:
+        config = json.load(config_file)
+except:
+    config = {}
+
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-af'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SECRET_KEY = config.get('SECRET_KEY') or 'secret-af'
+    SQLALCHEMY_DATABASE_URI = config.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
